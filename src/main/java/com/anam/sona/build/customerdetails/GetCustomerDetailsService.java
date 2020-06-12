f77290a6700f8cee6415c82a804a39d14629a0ea
Navya Damisetti <navyaratna.deepika14@gmail.com>
2020-06-12 20:23:23 +0530

    cleanup

--- src/main/java/com/anam/sona/build/customerdetails/GetCustomerDetailsService.java
+++ src/main/java/com/anam/sona/build/customerdetails/GetCustomerDetailsService.java
@@ -8,8 +8,6 @@ public class GetCustomerDetailsService {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GetCustomerDetailsService.class, args);
-		
-		System.out.println("Hello World");
 	}
 
 }
